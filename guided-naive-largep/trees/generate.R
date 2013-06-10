9d29918cb1e2bc68bd8f1a52b0375e3328845e03
Connor McCoy <cmccoy@fhcrc.org>
2013-06-10 12:15:38 -0700

    Add guided vs naive

---
+++ guided-naive-largep/trees/generate.R
@@ -0,0 +1,18 @@
+#!/usr/bin/env Rscript
+
+library(ape)
+
+set.seed(1)
+
+n_trees_per_taxon_count <- 5
+n_taxa <- 10
+
+r <- lapply(as.list(n_taxa), function(i) {
+  lapply(as.list(1:n_trees_per_taxon_count), function(j) {
+    path <- sprintf('%02dtaxon-%02d.nwk', i, j)
+    write.tree(rtree(i, br=rexp, rate=10),
+               path)
+    message(path)
+    path
+  })
+})
