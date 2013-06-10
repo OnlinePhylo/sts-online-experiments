a81144e4b3b3094915374f611e1c7f4df995b5f3
Connor McCoy <cmccoy@fhcrc.org>
2013-06-10 13:16:37 -0700

    pp_comp

--- guided-naive-largep/bin/pp_comparison.R
+++ guided-naive-largep/bin/pp_comparison.R
@@ -14,10 +14,10 @@ pp <- transform(pp, tree=sub('\\.nwk$', '', basename(tree)))
 
 theme_set(theme_bw(16))
 
-p <- ggplot(pp, aes(x=pp1, y=pp2, color=type, shape=type)) +
+p <- ggplot(pp, aes(x=pp1, y=pp2, color=type, shape=move_type)) +
   geom_abline(slope=1, color='grey') +
   geom_point(alpha=0.7, size=1.4) +
-  facet_grid(move_type~tree) +
+  facet_grid(trim_taxon~tree) +
   xlim(0, 1) +
   ylim(0, 1) +
   xlab("Split posterior probability 1 (MrBayes)") +
@@ -25,4 +25,4 @@ p <- ggplot(pp, aes(x=pp1, y=pp2, color=type, shape=type)) +
   theme(legend.position='bottom',
         axis.text.x=element_text(angle=35, hjust=1)) +
   ggtitle("Split posterior probability comparison")
-ggsave(outfile, plot=p, width=11, height=6)
+ggsave(outfile, plot=p, width=11, height=11)
