71b11c1f8a6ed64c6c46184ceb95253b4af288eb
Brian Claywell <bclaywel@fhcrc.org>
2016-10-06 11:21:28 -0700

    Add hacky fix for environment module library path.

--- env.sh
+++ env.sh
@@ -7,3 +7,7 @@ module load bali-phy/2.1.1
 module load nlopt/2.4.2
 
 export R_LIBS=$PWD/venv/lib/R
+
+# the environment modules aren't updating LD_LIBRARY_PATH properly, so hack in a fix
+export LD_LIBRARY_PATH="$LD_RUN_PATH"
+
